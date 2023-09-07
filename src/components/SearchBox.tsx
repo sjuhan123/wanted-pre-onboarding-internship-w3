b94a5e2b21a2b8567a3c12a900f65abdb5cf5c9b
sjuhan123 <sjuhan123@gmail.com>
2023-09-07 23:07:03 +0900

    REFACTOR: SearchBox 컴포넌트 리팩토링

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,47 +1,119 @@
-import React from 'react';
-import styled from 'styled-components';
-import Suggestion from './Suggestion';
+import React, { useRef, useLayoutEffect } from 'react';
+import { styled } from 'styled-components';
+import { SickInfo, SickInfos } from '../types';
+import { isEmptyArray } from '../utils/isEmptyArray';
+import HighlightedText from './HighlightedText';
+import { StyledSearchIcon } from './Search';
 
-export default function SearchBox() {
+interface Iprops {
+  searchList: SickInfos;
+  selectedIndex: number;
+  setSelectedIndex: React.Dispatch<React.SetStateAction<number>>;
+  selectListItemByKeyArrow: (e: React.KeyboardEvent<HTMLLIElement>) => void;
+  inputValue: string;
+}
+
+export default function SearchBox(props: Iprops) {
+  const { searchList, selectedIndex, setSelectedIndex, selectListItemByKeyArrow, inputValue } =
+    props;
+
+  const liRef = useRef<HTMLLIElement | null>(null);
+
+  useLayoutEffect(() => {
+    if (liRef.current) {
+      liRef.current.focus();
+    }
+  });
+
+  const resetSelectedIndex = (idx: number) => {
+    setSelectedIndex(idx);
+  };
+
   return (
-    <SearchContainer>
-      <RecentSearchBox>
-        <p>최근 검색어</p>
-        <Suggestion suggestion='비만' />
-      </RecentSearchBox>
-      <SuggestionBox>
-        <p>추천 검색어</p>
-        <Suggestion suggestion='비형감염' />
-      </SuggestionBox>
-    </SearchContainer>
+    <SearchResultContainer>
+      <ResultTitle>추천 검색어</ResultTitle>
+      <ResultBox>
+        <ul>
+          {isEmptyArray(searchList) ? (
+            <Message>검색어 없음</Message>
+          ) : (
+            searchList.map((el: SickInfo, idx: number) => (
+              <SearchText
+                key={el.sickCd}
+                ref={idx === selectedIndex ? liRef : null}
+                tabIndex={selectedIndex === idx ? 0 : -1}
+                onKeyDown={selectListItemByKeyArrow}
+                onClick={() => resetSelectedIndex(idx)}
+              >
+                <StyledSearchIcon width='16px' height='16px' />
+                {el.sickNm.includes(inputValue) ? (
+                  <HighlightedText parts={el.sickNm.split(inputValue)} value={inputValue} />
+                ) : (
+                  <span>{el.sickNm}</span>
+                )}
+              </SearchText>
+            ))
+          )}
+        </ul>
+      </ResultBox>
+    </SearchResultContainer>
   );
 }
 
-const SearchContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 12px;
+const SearchResultContainer = styled.div`
+  width: 490px;
+  max-height: 250px;
+  position: absolute;
+  top: 360px;
+  padding: 18px;
+  background-color: var(--white);
   border-radius: 20px;
-  background-color: #ffffff;
-  width: 100%;
-  height: auto;
-  padding: 16px;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
 
-  p {
-    color: #818181;
-    font-size: 0.875rem;
-    font-weight: 500;
-    margin: 0;
-    font-family: inherit;
+const ResultTitle = styled.h3`
+  font-size: 12px;
+  font-weight: normal;
+  color: var(--deepGray);
+`;
+
+const ResultBox = styled.div`
+  max-height: 160px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: var(--blue);
+    border-radius: 50px;
+    border: 4px solid var(--blue);
+    min-height: 50px;
   }
 `;
 
-const RecentSearchBox = styled.div`
-  border-bottom: 2px solid #e0e0e0;
-  background-color: #ffffff;
+const SearchText = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 8px 0;
+
+  span {
+    padding: 5px;
+    flex: 1;
+  }
+
+  &:focus,
+  &:focus-visible {
+    outline: none;
+    color: black;
+    font-weight: 500;
+    background-color: rgba(49, 130, 246, 0.2);
+    border-radius: 5px;
+  }
 `;
 
-const SuggestionBox = styled.div`
-  padding-top: 16px;
+const Message = styled.p`
+  color: var(--deepGray);
+  text-align: center;
 `;
