3e2674479a71e66aa97ee0bd1bc2383c3b24dd9e
sjuhan123 <sjuhan123@gmail.com>
2023-09-07 23:06:45 +0900

    FEAT: SearchBar 컴포넌트 리팩토링

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,80 +1,73 @@
 import React from 'react';
-import styled from 'styled-components';
+import { styled } from 'styled-components';
+import { StyledSearchIcon } from './Search';
 
-interface SearchBarProps {
+interface Iprops {
   inputValue: string;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleInputFocus: (isFocus: boolean) => void;
-  isInputFocused: boolean;
+  setSelectedIndex: React.Dispatch<React.SetStateAction<number>>;
+  selectListItemByKeyArrow: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function SearchBar({
-  inputValue,
-  handleInputChange,
-  handleInputFocus,
-  isInputFocused,
-}: SearchBarProps) {
+export default function SearchBar(props: Iprops) {
+  const { inputValue, setSelectedIndex, selectListItemByKeyArrow, changeHandler } = props;
+
+  const resetSelectedIndex = () => {
+    setSelectedIndex(-1);
+  };
+
   return (
-    <SearchBarBox isInputFocused={isInputFocused}>
-      <div>🔍</div>
-      <input
+    <SearchBox>
+      <StyledSearchIcon width='16px' height='16px' />
+      <SearchInput
+        type='search'
+        placeholder='검색어를 입력해 주세요'
         value={inputValue}
-        onChange={handleInputChange}
-        placeholder='질환명을 입력해 주세요.'
-        onFocus={() => handleInputFocus(true)}
-        onBlur={() => handleInputFocus(false)}
+        onKeyDown={selectListItemByKeyArrow}
+        onChange={changeHandler}
+        onClick={resetSelectedIndex}
       />
-      <button>검색</button>
-    </SearchBarBox>
+      <SearchButton aria-label='검색'>
+        <StyledSearchIcon width='21px' height='21px' />
+      </SearchButton>
+    </SearchBox>
   );
 }
 
-interface SearchBarBox {
-  isInputFocused: boolean;
-}
-
-const SearchBarBox = styled.div<SearchBarBox>`
-  border-radius: 42px;
-  border: 2px solid;
-  border-color: ${({ isInputFocused }) => (isInputFocused ? '#007be9' : '#ffffff')};
-  background-color: #ffffff;
-  align-items: center;
-  font-size: 1rem;
-  font-weight: 400;
-  letter-spacing: -0.018em;
-  line-height: 1.6;
+const SearchBox = styled.div`
   display: flex;
-  width: 100%;
-  padding: 0px 16px;
+  align-items: center;
+  width: 490px;
+  background-color: var(--white);
+  padding: 20px 10px 20px 24px;
+  border-radius: 42px;
+`;
 
-  input {
-    border: none;
-    background-color: transparent;
-    padding: 0 8px;
-    flex: 1;
-    height: 48px;
-    font-size: 1rem;
-    font-weight: 400;
+const SearchInput = styled.input`
+  flex: 1;
+  caret-color: var(--blue);
+  padding-right: 25px;
+  background-color: var(--white);
+  font-size: 18px;
+  outline: 0;
+
+  &::placeholder {
     font-family: inherit;
-    outline: none;
-
-    &::placeholder {
-      color: #c4c4c4;
-    }
+    color: var(--gray);
   }
+`;
 
-  button {
-    border: none;
-    border-radius: 100%;
-    background-color: #007be9;
-    width: 36px;
-    height: 36px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    outline: none;
-    cursor: pointer;
-    color: #ffffff;
-    font-weight: 1000;
+const SearchButton = styled.button`
+  width: 48px;
+  height: 48px;
+  background-color: var(--blue);
+  border-radius: 50%;
+
+  svg {
+    margin: 0;
+
+    path {
+      fill: var(--white);
+    }
   }
 `;
