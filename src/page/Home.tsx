6eeaa954c8a04f4628f7bd8192b9419b342c39f5
sjuhan123 <sjuhan123@gmail.com>
2023-09-07 23:08:26 +0900

    REFACTOR: Home 컴포넌트 리팩토링 및 불러오는 컴포넌트 수정

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,53 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
-import SearchBar from '../components/SearchBar';
-import SearchBox from '../components/SearchBox';
-import useInput from '../hooks/useInput';
+import Search from '../components/Search';
 
 export default function Home() {
-  const { inputValue, handleInputChange, isInputFocused, handleInputFocus } = useInput();
-
   return (
     <Layout>
-      <main>
-        <header>
-          국내 모든 임상시험 검색하고 <br />
-          온라인으로 참여하기
-        </header>
-        <SearchBar
-          inputValue={inputValue}
-          handleInputChange={handleInputChange}
-          handleInputFocus={handleInputFocus}
-          isInputFocused={isInputFocused}
-        />
-        {isInputFocused && <SearchBox />}
-      </main>
+      <header>
+        국내 모든 임상시험 검색하고 <br />
+        온라인으로 참여하기
+      </header>
+      <Search />
     </Layout>
   );
 }
 
 const Layout = styled.div`
-  background-color: rgb(150, 239, 238);
-  height: 50vh;
+  position: relative;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
-
-  main {
-    width: 100%;
-    max-width: 360px;
-    padding: 100px 0 290px;
-    height: 100%;
-  }
+  align-items: center;
+  padding: 80px 0;
 
   header {
-    font-size: 1.25rem;
+    font-size: 2.125rem;
     font-weight: 700;
     letter-spacing: -0.018em;
     line-height: 1.6;
-    margin: 0;
-    margin-bottom: 20px;
-    font-family: inherit;
     text-align: center;
+    margin-bottom: 42px;
   }
 `;
