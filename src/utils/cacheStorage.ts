bb7aeee3d02392055877d7d8f6ff325dbe93a454
sjuhan123 <sjuhan123@gmail.com>
2023-09-07 23:05:48 +0900

    FEAT: cacheStorage 관련 함수들 구현

--- src/utils/cacheStorage.ts
+++ src/utils/cacheStorage.ts
@@ -1,18 +1,51 @@
-import { SickInfos } from '../types';
+export async function getFromCacheStorage(keyword: string) {
+  try {
+    const response = await cacheStorage.get(keyword);
 
-export class CacheStorage<D> {
-  private _storage: Map<string, D>;
-  constructor() {
-    this._storage = new Map();
-  }
+    if (!(response && response.ok)) {
+      return [];
+    }
+    const res = await response.json();
 
-  set(key: string, value: D) {
-    this._storage.set(key, value);
-  }
+    const currentTime = new Date().getTime();
+    const cachedTime = res.timestamp;
+    const timeDiff = currentTime - cachedTime;
 
-  get(key: string) {
-    return this._storage.get(key);
+    if (timeDiff <= res.expireTime) {
+      return res.data;
+    }
+    return [];
+  } catch (error) {
+    console.error('Error while getting data from cache:', error);
+    return [];
   }
 }
 
-export const cacheStorage = new CacheStorage<SickInfos>();
+export async function setToCacheStorage<T>(keyword: string, response: T, expireTime: number) {
+  const res = { expireTime, data: response, timestamp: new Date().getTime() };
+  const clonedResponse = new Response(JSON.stringify(res));
+  await cacheStorage.put(keyword, clonedResponse);
+}
+
+class CacheStorage {
+  private KEY: string;
+  private cachePromise: Promise<Cache>;
+
+  constructor(KEY: string) {
+    this.KEY = KEY;
+    this.cachePromise = caches.open(this.KEY);
+  }
+
+  async put(url: string, response: Response): Promise<void> {
+    const cache = await this.cachePromise;
+    await cache.put(url, response);
+  }
+
+  async get(url: string): Promise<Response | undefined> {
+    const cache = await this.cachePromise;
+    return await cache.match(url);
+  }
+}
+
+const KEY = 'disease';
+export const cacheStorage = new CacheStorage(KEY);
