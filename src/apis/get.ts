72b85e5388b0bb2d6206a7fd9b46068d219325b5
sjuhan123 <sjuhan123@gmail.com>
2023-09-07 23:04:06 +0900

    FEAT: 비동기 로직 axios로 변경

--- src/apis/get.ts
+++ src/apis/get.ts
@@ -1,13 +1,24 @@
-import { API_METHOD } from '../constants/apis';
+import axios from 'axios';
+import { BASE_URL, API_PATH } from '../constants/apis';
 
-export const getAPI = async (url: string) => {
-  try {
-    console.info('calling api');
-    const res = await fetch(url, {
-      method: API_METHOD.GET,
-    });
-    return res.json();
-  } catch (e) {
-    console.error(e);
-  }
+export const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  console.info('calling api');
+
+  return config;
+});
+
+export const getRecommendedWord = async (keyword: string) => {
+  const response = await instance.get(API_PATH.SICK, {
+    params: {
+      q: keyword,
+    },
+  });
+  return response.data;
 };
