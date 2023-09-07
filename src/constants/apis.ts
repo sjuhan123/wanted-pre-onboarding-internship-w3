dcc9e9360439dbc4eb113dbbcf98cc9e2b8d49b9
sjuhan123 <sjuhan123@gmail.com>
2023-09-07 11:07:08 +0900

    FEAT: constants API 관련 상수 구현

---
+++ src/constants/apis.ts
@@ -0,0 +1,9 @@
+export const API_BASE_URL = 'http://localhost:4000/' as const;
+
+export const API_PATH = Object.freeze({
+  SICK: 'sick',
+});
+
+export const API_METHOD = Object.freeze({
+  GET: 'get',
+});
